ac00d9a76aa443f918e079a6f1df947067d4ece7
Lionel GUEZ <guez@lmd.ens.fr>
2021-01-13 21:54:05 +0100

    Remove `_t_defs` and `_all_definitions`
    We do not use these variables.

--- TAGS.cmake
+++ TAGS.cmake
@@ -19,26 +19,19 @@ function(tags_add_dir _dir)
   foreach(_tgt ${_targets_list})
     get_property(_t_files  TARGET ${_tgt} PROPERTY SOURCES)
     list(FILTER _t_files EXCLUDE REGEX TARGET_OBJECTS)
-    get_property(_t_defs   TARGET ${_tgt} PROPERTY
-      COMPILE_DEFINITIONS)
     
     list(APPEND _all_source_files ${_t_files})
     string(REGEX REPLACE "${CMAKE_SOURCE_DIR}/" "" _t_files
       "${_t_files}")
     string(REGEX REPLACE ";" "\n" _t_files "${_t_files}")
-    
-    string(REGEX REPLACE ";" "\n" _t_defs "${_t_defs}")
-    string(REGEX REPLACE "^([^=]+)$" "\\1=1" _t_defs "${_t_defs}")
   endforeach()
 
   set(_all_source_files ${_all_source_files} PARENT_SCOPE)
-  set(_all_definitions ${_all_definitions} PARENT_SCOPE)
 endfunction()
 
 if(CTAGS)
   set(SOURCES_LIST "${CMAKE_BINARY_DIR}/sources_list")
   set(_all_source_files "")
-  set(_all_definitions "")
   file(REMOVE "${SOURCES_LIST}")
   tags_add_dir(${CMAKE_SOURCE_DIR})
   list(SORT _all_source_files)
@@ -49,11 +42,6 @@ if(CTAGS)
 
   string(REGEX REPLACE ";" "\n" _asf "${_asf}")
   file(APPEND ${SOURCES_LIST} "\n${_asf}")
-  string(REGEX REPLACE ";" "\n" _all_definitions "${_all_definitions}")
-
-  string(REGEX REPLACE "^([^=]+)$" "\\1=1" _all_definitions
-    "${_all_definitions}")
-
   add_custom_target(TAGS DEPENDS ${CMAKE_SOURCE_DIR}/TAGS)
 
   add_custom_command(OUTPUT ${CMAKE_SOURCE_DIR}/TAGS
