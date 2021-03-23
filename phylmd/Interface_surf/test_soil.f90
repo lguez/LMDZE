8fa9373dd6b2e6988e326c4a08f406ce5452569c
Lionel GUEZ <guez@lmd.ens.fr>
2021-03-23 12:17:13 +0100

    Set up MPI and XIOS in `test_soil`
    We need to set up MPI and XIOS in `test_soil` because `conf_gcm` calls XIOS.

--- phylmd/Interface_surf/test_soil.f90
+++ phylmd/Interface_surf/test_soil.f90
@@ -1,6 +1,10 @@
 program test_soil
 
   use jumble, only: new_unit
+  use mpi_f08, only: mpi_init, mpi_finalize, mpi_comm_size, mpi_comm_world, &
+       mpi_abort
+  use xios, only: xios_initialize, xios_finalize, xios_context_initialize, &
+       xios_context_finalize, xios_close_context_definition
 
   use conf_gcm_m, only: conf_gcm
   USE dimsoil, only: nsoilmx
@@ -19,13 +23,20 @@ program test_soil
   REAL soilflux(1) ! (knon) 
   ! surface diffusive flux from ground (W m-2)
 
-  integer unit, i
+  integer unit, i, n_proc, return_comm
 
   !----------------------------------------------------------------
+
+  call mpi_init
+  call mpi_comm_size(mpi_comm_world, n_proc)
+  if (n_proc /= 1) call mpi_abort(mpi_comm_world, errorcode = 1)
+  call xios_initialize("LMDZE", return_comm = return_comm)
+  CALL xios_context_initialize("LMDZE_context", return_comm)
 
   call set_unit_nml
   open(unit_nml, file="used_namelists.txt", status="replace", action="write")
   CALL conf_gcm
+  call xios_close_context_definition
   tsoil = 270.
   call soil(is_ter, [0.], [288.], tsoil, soilcap, soilflux)
   call new_unit(unit)
@@ -40,5 +51,8 @@ program test_soil
   close(unit)
   print *, "soilcap = ", soilcap
   print *, "soilflux = ", soilflux
+  CALL xios_context_finalize
+  call xios_finalize
+  call mpi_finalize
 
 end program test_soil
