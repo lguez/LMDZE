6f9b3083bb7a704796905ad2d2693401f3e0e2bb
Lionel GUEZ <guez@lmd.ens.fr>
2021-03-25 22:29:14 +0100

    Extract dynamics part to new procedure etat0dyn
    Following LMDZ. Note that `tsol_2d` must pass from etat0 to etat0dyn.

--- dynphy_lonlat/ce0l.f90
+++ dynphy_lonlat/ce0l.f90
@@ -10,6 +10,7 @@ PROGRAM ce0l
   use dynetat0_chosen_m, only: read_serre
   use dynetat0_m, only: fyhyp, fxhyp
   use etat0_m, only: etat0
+  use etat0dyn_m, only: etat0dyn
   use grilles_gcm_netcdf_sub_m, only: grilles_gcm_netcdf_sub
   use indicesol, only: nbsrf
   use infotrac_init_m, only: infotrac_init
@@ -20,6 +21,9 @@ PROGRAM ce0l
   use unit_nml_m, only: unit_nml, set_unit_nml
 
   implicit none
+
+  real, ALLOCATABLE:: tsol_2d(:, :) ! (iim + 1, jjm + 1)
+  ! both soil temperature and surface temperature, in K
 
   REAL, ALLOCATABLE:: phis(:, :) ! (iim + 1, jjm + 1)
   ! surface geopotential, in m2 s-2
@@ -35,7 +39,7 @@ PROGRAM ce0l
   call set_dimensions
   call paramet
   call init_dimphy
-  ALLOCATE(phis(iim + 1, jjm + 1))
+  ALLOCATE(tsol_2d(iim + 1, jjm + 1), phis(iim + 1, jjm + 1))
   ALLOCATE(pctsrf(klon, nbsrf))
   call read_comdissnew
   call read_serre
@@ -49,7 +53,8 @@ PROGRAM ce0l
   CALL inifilr
 
   call infotrac_init
-  CALL etat0(phis, pctsrf)
+  CALL etat0(tsol_2d, phis, pctsrf)
+  CALL etat0dyn(tsol_2d, phis)
   CALL limit(pctsrf)
   CALL grilles_gcm_netcdf_sub(phis)
   close(unit_nml)
--- dynphy_lonlat/etat0.f90
+++ dynphy_lonlat/etat0.f90
@@ -4,27 +4,18 @@ module etat0_m
 
 contains
 
-  SUBROUTINE etat0(phis, pctsrf)
+  SUBROUTINE etat0(tsol_2d, phis, pctsrf)
 
     ! From "etat0_netcdf.F", version 1.3, 2005/05/25 13:10:09
 
-    use caldyn0_m, only: caldyn0
-    use comconst, only: cpp, kappa
-    use comgeom, only: aire_2d, apoln, apols, cu_2d, cv_2d
-    use dimensions, only: iim, jjm, llm, nqmx
+    use dimensions, only: iim, jjm, llm
     use dimphy, only: klon
     use dimsoil, only: nsoilmx
-    use disvert_m, only: ap, bp, preff
-    use dynetat0_m, only: rlatu, rlatv, rlonu, rlonv
+    use dynetat0_m, only: rlatu, rlonv
     use dynetat0_chosen_m, only: day_ref
-    use dynredem0_m, only: dynredem0
-    use dynredem1_m, only: dynredem1
-    use exner_hyb_m, only: exner_hyb
-    use geopot_m, only: geopot
     use grille_m_m, only: grille_m
     use grid_change, only: init_dyn_phy, dyn_phy
     use indicesol, only: is_oce, is_sic, is_ter, is_lic, epsfra, nbsrf
-    use massdair_m, only: massdair
     use netcdf, only: nf90_nowrite
     use netcdf95, only: nf95_close, nf95_get_var, nf95_gw_var, nf95_put_var, &
          nf95_inq_varid, nf95_open
@@ -32,13 +23,11 @@ contains
     use phyetat0_m, only: masque, set_lat, set_lon, set_masque
     use phyredem0_m, only: phyredem0, ncid_restartphy
     use phyredem_m, only: phyredem
-    use q_sat_m, only: q_sat
-    use regr_lat_time_coefoz_m, only: regr_lat_time_coefoz
-    use regr_pr_o3_m, only: regr_pr_o3
-    use start_init_dyn_m, only: start_init_dyn
     USE start_init_orog_m, only: start_init_orog
     use start_init_phys_m, only: start_init_phys
-    use start_inter_3d_m, only: start_inter_3d
+
+    real, intent(out):: tsol_2d(:, :) ! (iim + 1, jjm + 1)
+    ! both soil temperature and surface temperature, in K
 
     REAL, intent(out):: phis(:, :) ! (iim + 1, jjm + 1)
     ! surface geopotential, in m2 s-2
@@ -48,14 +37,6 @@ contains
     ! position "i".
 
     ! Local:
-
-    REAL, dimension(iim + 1, jjm + 1, llm):: ucov, t3d, teta
-    REAL vcov(iim + 1, jjm, llm)
-
-    REAL q(iim + 1, jjm + 1, llm, nqmx)
-    ! Mass fractions of trace species. "q(i, j, l)" is at longitude
-    ! "rlonv(i)", latitude "rlatu(j)" and pressure level "pls(i, j,
-    ! l)".
 
     REAL fqsurf(klon, nbsrf), fsnow(klon, nbsrf) 
     REAL falbe(klon, nbsrf)
@@ -68,11 +49,7 @@ contains
     real, dimension(iim + 1, jjm + 1):: zmea_2d, zstd_2d, zsig_2d, zgam_2d
     real, dimension(iim + 1, jjm + 1):: zthe_2d, zpic_2d, zval_2d
 
-    real tsol_2d(iim + 1, jjm + 1)
-    ! both soil temperature and surface temperature, in K
-
     real qsol_2d(iim + 1, jjm + 1) ! column-density of water in soil, in kg m-2
-    real ps(iim + 1, jjm + 1)
     REAL zmea(klon), zstd(klon)
     REAL zsig(klon), zgam(klon)
     REAL zthe(klon)
@@ -87,23 +64,9 @@ contains
     REAL, ALLOCATABLE:: landice(:, :) ! fraction land ice
     REAL flic_tmp(iim + 1, jjm + 1) ! fraction land ice temporary
 
-    INTEGER l, ji
-
-    REAL pk(iim + 1, jjm + 1, llm) ! fonction d'Exner aux milieux des couches 
-    real pks(iim + 1, jjm + 1)
-    REAL masse(iim + 1, jjm + 1, llm)
-    REAL phi(iim + 1, jjm + 1, llm)
+    INTEGER ji
     real sig1(klon, llm) ! section adiabatic updraft
     real w01(klon, llm) ! vertical velocity within adiabatic updraft
-
-    real pls(iim + 1, jjm + 1, llm)
-    ! (pressure at mid-layer of LMDZ grid, in Pa)
-    ! "pls(i, j, l)" is at longitude "rlonv(i)", latitude "rlatu(j)",
-    ! for layer "l")
-
-    REAL p3d(iim + 1, jjm + 1, llm+1) ! pressure at layer interfaces, in Pa
-    ! ("p3d(i, j, l)" is at longitude "rlonv(i)", latitude "rlatu(j)",
-    ! for interface "l")
 
     !---------------------------------
 
@@ -226,72 +189,6 @@ contains
          sollw, null_array, null_array, frugs, agesno, zmea, zstd, zsig, zgam, &
          zthe, zpic, zval, t_ancien, q_ancien, rnebcon, ratqs, clwcon, &
          null_array, sig1, w01)
-
-    CALL start_init_dyn(tsol_2d, phis, ps)
-
-    ! Compute pressure on intermediate levels:
-    forall(l = 1: llm + 1) p3d(:, :, l) = ap(l) + bp(l) * ps
-    CALL exner_hyb(ps, p3d, pks, pk)
-    call assert(MINVAL(pk) /= MAXVAL(pk), '"pk" should not be constant')
-
-    pls = preff * (pk / cpp)**(1. / kappa)
-    PRINT *, "minval(pls) = ", minval(pls)
-    print *, "maxval(pls) = ", maxval(pls)
-
-    call start_inter_3d('U', rlonv, rlatv, pls, ucov)
-    forall (l = 1: llm) ucov(:iim, :, l) = ucov(:iim, :, l) * cu_2d(:iim, :)
-    ucov(iim+1, :, :) = ucov(1, :, :)
-
-    call start_inter_3d('V', rlonu, rlatu(:jjm), pls(:, :jjm, :), vcov)
-    forall (l = 1: llm) vcov(:iim, :, l) = vcov(:iim, :, l) * cv_2d(:iim, :)
-    vcov(iim + 1, :, :) = vcov(1, :, :)
-
-    call start_inter_3d('TEMP', rlonu, rlatv, pls, t3d)
-    PRINT *, 'minval(t3d) = ', minval(t3d)
-    print *, "maxval(t3d) = ", maxval(t3d)
-
-    teta(:iim, :, :) = t3d(:iim, :, :) * cpp / pk(:iim, :, :)
-    teta(iim + 1, :, :) = teta(1, :, :)
-    DO l = 1, llm
-       teta(:, 1, l) = SUM(aire_2d(:, 1) * teta(:, 1, l)) / apoln
-       teta(:, jjm + 1, l) = SUM(aire_2d(:, jjm + 1) * teta(:, jjm + 1, l)) &
-            / apols
-    ENDDO
-
-    ! Water vapor:
-    call start_inter_3d('R', rlonu, rlatv, pls, q(:, :, :, 1))
-    q(:, :, :, 1) = 0.01 * q(:, :, :, 1) * q_sat(t3d, pls)
-    WHERE (q(:, :, :, 1) < 0.) q(:, :, :, 1) = 1E-10
-    
-    forall (l = 1:llm)
-       q(:, 1, l, 1) = SUM(aire_2d(:, 1) * q(:, 1, l, 1)) / apoln
-       q(:, jjm + 1, l, 1) &
-            = SUM(aire_2d(:, jjm + 1) * q(:, jjm + 1, l, 1)) / apols
-    END forall
-
-    q(:, :, :, 2:4) = 0. ! liquid water, radon and lead
-
-    if (nqmx >= 5) then
-       ! Ozone:
-       call regr_lat_time_coefoz
-       call regr_pr_o3(p3d, q(:, :, :, 5))
-       ! Convert from mole fraction to mass fraction:
-       q(:, :, :, 5) = q(:, :, :, 5) * 48. / 29.
-    end if
-
-    ! Calcul interm\'ediaire :
-    CALL massdair(p3d, masse)
-
-    forall (l = 1:llm)
-       masse(:, 1, l) = SUM(aire_2d(:iim, 1) * masse(:iim, 1, l)) / apoln
-       masse(:, jjm + 1, l) = &
-            SUM(aire_2d(:iim, jjm + 1) * masse(:iim, jjm + 1, l)) / apols
-    END forall
-
-    CALL geopot(teta, pk , pks, phis, phi)
-    CALL caldyn0(ucov, vcov, teta, ps, pk, phis, phi)
-    CALL dynredem0(phis, day_ref)
-    CALL dynredem1(vcov, ucov, teta, q, masse, ps, itau = 0)
 
   END SUBROUTINE etat0
 
