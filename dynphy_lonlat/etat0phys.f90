16d1d02f68ac6fcee16bea596e00a2c24e7fc6eb
Lionel GUEZ <guez@lmd.ens.fr>
2021-03-25 23:23:33 +0100

    Extract part of etat0phys to `start_init_subsurf`
    Extract part of etat0phys to new procedure
    `start_init_subsurf`. Following LMDZ revision 2337.

--- dynphy_lonlat/etat0phys.f90
+++ dynphy_lonlat/etat0phys.f90
@@ -8,24 +8,20 @@ contains
 
     ! From "etat0_netcdf.F", version 1.3, 2005/05/25 13:10:09
 
-    use netcdf, only: nf90_nowrite
-    use netcdf95, only: nf95_close, nf95_get_var, nf95_gw_var, nf95_put_var, &
-         nf95_inq_varid, nf95_open
-    use nr_util, only: pi, assert
+    use netcdf95, only: nf95_put_var, nf95_inq_varid
 
     use dimensions, only: iim, jjm, llm
     use dimphy, only: klon
     use dimsoil, only: nsoilmx
-    use dynetat0_m, only: rlatu, rlonv
     use dynetat0_chosen_m, only: day_ref
-    use grille_m_m, only: grille_m
     use grid_change, only: init_dyn_phy, dyn_phy
-    use indicesol, only: is_oce, is_sic, is_ter, is_lic, epsfra, nbsrf
-    use phyetat0_m, only: masque, set_lat, set_lon, set_masque
+    use indicesol, only: is_oce, is_sic, is_ter, is_lic, nbsrf
+    use phyetat0_m, only: set_lat, set_lon, set_masque
     use phyredem0_m, only: phyredem0, ncid_restartphy
     use phyredem_m, only: phyredem
     USE start_init_orog_m, only: start_init_orog
     use start_init_phys_m, only: start_init_phys
+    use start_init_subsurf_m, only: start_init_subsurf
 
     real, intent(out):: tsol_2d(:, :) ! (iim + 1, jjm + 1)
     ! both soil temperature and surface temperature, in K
@@ -57,15 +53,7 @@ contains
     REAL zpic(klon), zval(klon)
     REAL t_ancien(klon, llm), q_ancien(klon, llm)
     real clwcon(klon, llm), rnebcon(klon, llm), ratqs(klon, llm)
-
-    ! D\'eclarations pour lecture glace de mer :
-    INTEGER iml_lic, jml_lic
-    INTEGER ncid, varid
-    REAL, ALLOCATABLE:: dlon_lic(:), dlat_lic(:)
-    REAL, ALLOCATABLE:: landice(:, :) ! fraction land ice
-    REAL flic_tmp(iim + 1, jjm + 1) ! fraction land ice temporary
-
-    INTEGER ji
+    INTEGER varid
     real sig1(klon, llm) ! section adiabatic updraft
     real w01(klon, llm) ! vertical velocity within adiabatic updraft
 
@@ -89,75 +77,7 @@ contains
     zthe = pack(zthe_2d, dyn_phy)
     zpic = pack(zpic_2d, dyn_phy)
     zval = pack(zval_2d, dyn_phy)
-
-    ! On initialise les sous-surfaces.
-    ! Lecture du fichier glace de terre pour fixer la fraction de terre 
-    ! et de glace de terre :
-
-    call nf95_open("landiceref.nc", nf90_nowrite, ncid)
-
-    call nf95_inq_varid(ncid, 'longitude', varid)
-    call nf95_gw_var(ncid, varid, dlon_lic)
-    iml_lic = size(dlon_lic)
-
-    call nf95_inq_varid(ncid, 'latitude', varid)
-    call nf95_gw_var(ncid, varid, dlat_lic)
-    jml_lic = size(dlat_lic)
-
-    call nf95_inq_varid(ncid, 'landice', varid)
-    ALLOCATE(landice(iml_lic, jml_lic))
-    call nf95_get_var(ncid, varid, landice)
-
-    call nf95_close(ncid)
-
-    ! Interpolation sur la grille T du mod\`ele :
-    PRINT *, 'Dimensions de "landiceref.nc"'
-    print *, "iml_lic = ", iml_lic
-    print *, "jml_lic = ", jml_lic
-
-    ! Si les coordonn\'ees sont en degr\'es, on les transforme :
-    IF (MAXVAL(dlon_lic) > pi) THEN
-       dlon_lic = dlon_lic * pi / 180.
-    ENDIF
-    IF (maxval(dlat_lic) > pi) THEN 
-       dlat_lic = dlat_lic * pi/ 180.
-    ENDIF
-
-    flic_tmp(:iim, :) = grille_m(dlon_lic, dlat_lic, landice, rlonv(:iim), &
-         rlatu)
-    flic_tmp(iim + 1, :) = flic_tmp(1, :)
-
-    ! Passage sur la grille physique :
-    pctsrf = 0.
-    pctsrf(:, is_lic) = pack(flic_tmp, dyn_phy)
-    
-    ! Ad\'equation avec le maque terre/mer :
-    WHERE (pctsrf(:, is_lic) < EPSFRA) pctsrf(:, is_lic) = 0.
-    WHERE (masque < EPSFRA) pctsrf(:, is_lic) = 0.
-    where (masque <= EPSFRA) pctsrf(:, is_ter) = masque
-    where (masque > EPSFRA)
-       where (pctsrf(:, is_lic) >= masque)
-          pctsrf(:, is_lic) = masque
-          pctsrf(:, is_ter) = 0.
-       elsewhere
-          pctsrf(:, is_ter) = masque - pctsrf(:, is_lic)
-          where (pctsrf(:, is_ter) < EPSFRA)
-             pctsrf(:, is_ter) = 0.
-             pctsrf(:, is_lic) = masque
-          end where
-       end where
-    end where
-
-    ! Sous-surface oc\'ean et glace de mer (pour d\'emarrer on met glace
-    ! de mer \`a 0) :
-    pctsrf(:, is_oce) = 1. - masque
-    WHERE (pctsrf(:, is_oce) < EPSFRA) pctsrf(:, is_oce) = 0.
-
-    ! V\'erification que la somme des sous-surfaces vaut 1 :
-    ji = count(abs(sum(pctsrf, dim = 2) - 1.) > EPSFRA)
-    IF (ji /= 0) then
-       PRINT *, 'Bad surface percentages for ', ji, 'points'
-    end IF
+    call start_init_subsurf(pctsrf)
 
     ! Initialisations :
     fsnow = 0.
---
+++ dynphy_lonlat/start_init_subsurf.f90
@@ -0,0 +1,110 @@
+module start_init_subsurf_m
+
+  implicit none
+
+contains
+
+  subroutine start_init_subsurf(pctsrf)
+    
+    ! From "etat0_netcdf.F", version 1.3, 2005/05/25 13:10:09
+
+    ! On initialise les sous-surfaces.
+    ! Lecture du fichier glace de terre pour fixer la fraction de terre 
+    ! et de glace de terre :
+
+    use netcdf, only: nf90_nowrite
+    use netcdf95, only: nf95_close, nf95_get_var, nf95_gw_var, nf95_inq_varid, &
+         nf95_open
+    use nr_util, only: pi
+
+    use dimensions, only: iim, jjm, llm
+    use dynetat0_m, only: rlatu, rlonv
+    use dynetat0_chosen_m, only: day_ref
+    use grille_m_m, only: grille_m
+    use grid_change, only: dyn_phy
+    use indicesol, only: is_oce, is_ter, is_lic, epsfra
+    use phyetat0_m, only: masque
+
+    REAL, intent(out):: pctsrf(:, :) ! (klon, nbsrf)
+    ! "pctsrf(i, :)" is the composition of the surface at horizontal
+    ! position "i".
+
+    ! Local:
+    INTEGER iml_lic, jml_lic
+    INTEGER ncid, varid
+    REAL, ALLOCATABLE:: dlon_lic(:), dlat_lic(:)
+    REAL, ALLOCATABLE:: landice(:, :) ! fraction land ice
+    REAL flic_tmp(iim + 1, jjm + 1) ! fraction land ice temporary
+    INTEGER ji
+
+    !---------------------------------
+
+    print *, "Call sequence information: start_init_subsurf"
+    call nf95_open("landiceref.nc", nf90_nowrite, ncid)
+
+    call nf95_inq_varid(ncid, 'longitude', varid)
+    call nf95_gw_var(ncid, varid, dlon_lic)
+    iml_lic = size(dlon_lic)
+
+    call nf95_inq_varid(ncid, 'latitude', varid)
+    call nf95_gw_var(ncid, varid, dlat_lic)
+    jml_lic = size(dlat_lic)
+
+    call nf95_inq_varid(ncid, 'landice', varid)
+    ALLOCATE(landice(iml_lic, jml_lic))
+    call nf95_get_var(ncid, varid, landice)
+
+    call nf95_close(ncid)
+
+    ! Interpolation sur la grille T du mod\`ele :
+    PRINT *, 'Dimensions de "landiceref.nc"'
+    print *, "iml_lic = ", iml_lic
+    print *, "jml_lic = ", jml_lic
+
+    ! Si les coordonn\'ees sont en degr\'es, on les transforme :
+    IF (MAXVAL(dlon_lic) > pi) THEN
+       dlon_lic = dlon_lic * pi / 180.
+    ENDIF
+    IF (maxval(dlat_lic) > pi) THEN 
+       dlat_lic = dlat_lic * pi/ 180.
+    ENDIF
+
+    flic_tmp(:iim, :) = grille_m(dlon_lic, dlat_lic, landice, rlonv(:iim), &
+         rlatu)
+    flic_tmp(iim + 1, :) = flic_tmp(1, :)
+
+    ! Passage sur la grille physique :
+    pctsrf = 0.
+    pctsrf(:, is_lic) = pack(flic_tmp, dyn_phy)
+
+    ! Ad\'equation avec le maque terre/mer :
+    WHERE (pctsrf(:, is_lic) < EPSFRA) pctsrf(:, is_lic) = 0.
+    WHERE (masque < EPSFRA) pctsrf(:, is_lic) = 0.
+    where (masque <= EPSFRA) pctsrf(:, is_ter) = masque
+    where (masque > EPSFRA)
+       where (pctsrf(:, is_lic) >= masque)
+          pctsrf(:, is_lic) = masque
+          pctsrf(:, is_ter) = 0.
+       elsewhere
+          pctsrf(:, is_ter) = masque - pctsrf(:, is_lic)
+          where (pctsrf(:, is_ter) < EPSFRA)
+             pctsrf(:, is_ter) = 0.
+             pctsrf(:, is_lic) = masque
+          end where
+       end where
+    end where
+
+    ! Sous-surface oc\'ean et glace de mer (pour d\'emarrer on met glace
+    ! de mer \`a 0) :
+    pctsrf(:, is_oce) = 1. - masque
+    WHERE (pctsrf(:, is_oce) < EPSFRA) pctsrf(:, is_oce) = 0.
+
+    ! V\'erification que la somme des sous-surfaces vaut 1 :
+    ji = count(abs(sum(pctsrf, dim = 2) - 1.) > EPSFRA)
+    IF (ji /= 0) then
+       PRINT *, 'Bad surface percentages for ', ji, 'points'
+    end IF
+
+  end subroutine start_init_subsurf
+
+end module start_init_subsurf_m
