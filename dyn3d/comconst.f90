a5e2171f1adb0c85bc81db928240fbee8c4ce7fa
Lionel GUEZ <guez@lmd.ens.fr>
2021-12-08 20:01:26 +0100

    Remove unused constant

--- dyn3d/comconst.f90
+++ dyn3d/comconst.f90
@@ -5,7 +5,4 @@ module comconst
   real, parameter:: daysec = 86400. ! number of seconds per day
   real, parameter:: ra = 6371229. ! radius of the Earth (in m)
 
-  real, parameter:: r = cpp * kappa
-  ! specific ideal gas constant for dry air, in J K-1 kg-1
-
 end module comconst
