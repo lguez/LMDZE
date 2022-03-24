6fd97fd560221511523eb08cdc4d45e4cba35bfb
Lionel GUEZ <guez@lmd.ens.fr>
2022-03-24 15:48:00 +0100

    Polish

--- phylmd/Orography/gwstress.f90
+++ phylmd/Orography/gwstress.f90
@@ -4,63 +4,51 @@ module gwstress_m
 
 contains
 
-  SUBROUTINE gwstress(nlon,nlev,ktest,kkenvh,prho,pstab,pvph, &
-       pstd,psig,pmea,ppic,ptau,pgeom1,pdmod)
+  SUBROUTINE gwstress(nlon, nlev, ktest, kkenvh, prho, pstab, pvph, &
+       pstd, psig, pmea, ppic, ptau, pgeom1, pdmod)
 
     !**** *gwstress*
 
-    !     purpose.
-    !     --------
+    ! purpose.
 
-    !**   interface.
-    !     ----------
-    !     call *gwstress*  from *gwdrag*
+    !** interface.
 
-    !        explicit arguments :
-    !        --------------------
-    !     ==== inputs ===
-    !     ==== outputs ===
+    ! call *gwstress* from *gwdrag*
 
-    !        implicit arguments :   none
-    !        --------------------
+    ! explicit arguments :
 
-    !     method.
-    !     -------
+    ! ==== inputs ===
+    ! ==== outputs ===
 
+    ! implicit arguments : none
 
-    !     externals.
-    !     ----------
+    ! method.
 
+    ! externals.
 
-    !     reference.
-    !     ----------
+    ! reference.
 
-    !        see ecmwf research department documentation of the "i.f.s."
+    ! see ecmwf research department documentation of the "i.f.s."
 
-    !     author.
-    !     -------
+    ! author.
 
-    !     modifications.
-    !     --------------
-    !     f. lott put the new gwd on ifs      22/11/93
+    ! modifications.
 
-    !-----------------------------------------------------------------------
+    ! f. lott put the new gwd on ifs 22/11/93
+
     USE dimensions
     USE dimphy
     USE suphec_m
     USE yoegwd
 
-    !-----------------------------------------------------------------------
-
-    !*       0.1   arguments
-    !              ---------
+    !* 0.1 arguments
 
     INTEGER nlon, nlev
     logical, intent(in):: ktest(nlon)
     integer, intent(in):: kkenvh(nlon)
 
-    REAL prho(nlon,nlev+1), pstab(nlon,nlev+1), ptau(nlon,nlev+1), &
-         pvph(nlon,nlev+1), pgeom1(nlon,nlev)
+    REAL prho(nlon, nlev+1), pstab(nlon, nlev+1), ptau(nlon, nlev+1), &
+         pvph(nlon, nlev+1), pgeom1(nlon, nlev)
     REAL, INTENT(IN):: pstd(nlon)
 
     REAL, INTENT(IN):: psig(nlon)
@@ -69,44 +57,39 @@ contains
 
     !-----------------------------------------------------------------------
 
-    !*       0.2   local arrays
-    !              ------------
+    !* 0.2 local arrays
+
     INTEGER jl
     REAL zblock, zvar, zeff
 
-    !-----------------------------------------------------------------------
+    !* 0.3 functions
 
-    !*       0.3   functions
-    !              ---------
-    !     ------------------------------------------------------------------
+    !* 1. initialization
 
-    !*         1.    initialization
-    !                --------------
-
-    !*         3.1     gravity wave stress.
+    !* 3.1 gravity wave stress.
 
     DO jl = 1, klon
        IF (ktest(jl)) THEN
 
-          !  effective mountain height above the blocked flow
+          ! effective mountain height above the blocked flow
 
           IF (kkenvh(jl)==klev) THEN
              zblock = 0.0
           ELSE
-             zblock = (pgeom1(jl,kkenvh(jl))+pgeom1(jl,kkenvh(jl)+1))/2./rg
+             zblock = (pgeom1(jl, kkenvh(jl))+pgeom1(jl, kkenvh(jl)+1))/2./rg
           END IF
 
           zvar = ppic(jl) - pmea(jl)
-          zeff = amax1(0.,zvar-zblock)
+          zeff = amax1(0., zvar-zblock)
 
-          ptau(jl,klev+1) = prho(jl,klev+1)*gkdrag*psig(jl)*zeff**2/4./ &
-               pstd(jl)*pvph(jl,klev+1)*pdmod(jl)*sqrt(pstab(jl,klev+1))
+          ptau(jl, klev+1) = prho(jl, klev+1)*gkdrag*psig(jl)*zeff**2/4./ &
+               pstd(jl)*pvph(jl, klev+1)*pdmod(jl)*sqrt(pstab(jl, klev+1))
 
-          !  too small value of stress or  low level flow include critical level
-          !  or low level flow:  gravity wave stress nul.
+          ! too small value of stress or low level flow include critical level
+          ! or low level flow: gravity wave stress nul.
        ELSE
 
-          ptau(jl,klev+1) = 0.0
+          ptau(jl, klev+1) = 0.0
 
        END IF
 
