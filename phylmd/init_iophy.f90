c3e18740fa19e0d7ea202089972c8d13a2cf52f6
Lionel GUEZ <guez@lmd.ens.fr>
2024-02-09 17:39:58 +0100

    Define grid for XIOS
    Strangely, the call to `xios_set_axis_attr` must be after the call to
    `xios_set_domain_attr`, else there is a segmentation fault.

--- phylmd/init_iophy.f90
+++ phylmd/init_iophy.f90
@@ -7,12 +7,16 @@ contains
   subroutine init_iophy
 
     ! Libraries:
-    use xios, only: xios_duration, xios_set_timestep, &
-         xios_close_context_definition, xios_set_time_origin, xios_date, &
-         xios_set_start_date, xios_define_calendar
+    use jumble, only: rad_to_deg
+    use xios, only: xios_set_axis_attr, xios_set_domain_attr, xios_duration, &
+         xios_set_timestep, xios_close_context_definition, &
+         xios_set_time_origin, xios_date, xios_set_start_date, &
+         xios_define_calendar
 
     use conf_gcm_m, only: dtphys
-    use dynetat0_m, only: day_ini
+    use dimensions, only: iim, jjm, llm
+    use disvert_m, only: presnivs
+    use dynetat0_m, only: rlatu, rlonv, day_ini
     use dynetat0_chosen_m, only: annee_ref, day_ref
 
     ! Local:
@@ -25,6 +29,12 @@ contains
     call xios_set_timestep(dtime)
     call xios_set_time_origin(xios_date(annee_ref, 1,day_ref, 0, 0, 0))
     CALL xios_set_start_date(xios_date(annee_ref, 1, day_ini, 0, 0, 0))
+    CALL xios_set_domain_attr("dom_glo", type = "rectilinear", &
+         ni_glo = iim, nj_glo = jjm + 1, &
+         lonvalue_1d = dble(rlonv(:iim) * rad_to_deg), &
+         latvalue_1d = dble(rlatu * rad_to_deg))
+    CALL xios_set_axis_attr("presnivs", n_glo = llm, &
+         value = dble(presnivs / 100.))
     call xios_close_context_definition
 
   end subroutine init_iophy
