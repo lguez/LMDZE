0380ef010f79414fe7ec077f8cf185499089ccab
Lionel GUEZ <guez@lmd.ens.fr>
2024-03-26 08:49:42 +0100

    Rename dummy arguments of procedure lw
    Remove initial letter p in the names of dummy arguments of procedure lw.

--- phylmd/Radlwsw/lw.f90
+++ phylmd/Radlwsw/lw.f90
@@ -4,9 +4,9 @@ module lw_m
 
 contains
 
-  SUBROUTINE LW(PPMB, PDP, PDT0, PEMIS, PTL, PTAVE, PWV, POZON, PCLDLD, &
-       PCLDLU, PVIEW, PCOLR, PCOLR0, PTOPLW, PSOLLW, PTOPLW0, PSOLLW0, &
-       psollwdown, plwup, plwdn, plwup0, plwdn0)
+  SUBROUTINE LW(PMB, DP, DT0, EMIS, TL, TAVE, WV, OZON, CLDLD, &
+       CLDLU, VIEW, COLR, COLR0, TOPLW, SOLLW, TOPLW0, SOLLW0, &
+       sollwdown, lwup, lwdn, lwup0, lwdn0)
 
     use comconst, only: daysec
     use dimphy, only: klon
@@ -39,32 +39,32 @@ contains
 
     ! Original : July 14th, 1989
 
-    DOUBLE PRECISION PCLDLD(KLON, LLM) ! DOWNWARD EFFECTIVE CLOUD COVER
-    DOUBLE PRECISION PCLDLU(KLON, LLM) ! UPWARD EFFECTIVE CLOUD COVER
-    DOUBLE PRECISION PDP(KLON, LLM) ! LAYER PRESSURE THICKNESS (Pa)
-    DOUBLE PRECISION PDT0(KLON) ! SURFACE TEMPERATURE DISCONTINUITY (K)
-    DOUBLE PRECISION PEMIS(KLON) ! SURFACE EMISSIVITY
-    DOUBLE PRECISION PPMB(KLON, LLM+1) ! HALF LEVEL PRESSURE (mb)
-    DOUBLE PRECISION POZON(KLON, LLM) ! O3 CONCENTRATION (kg/kg)
-    DOUBLE PRECISION PTL(KLON, LLM+1) ! HALF LEVEL TEMPERATURE (K)
+    DOUBLE PRECISION CLDLD(KLON, LLM) ! DOWNWARD EFFECTIVE CLOUD COVER
+    DOUBLE PRECISION CLDLU(KLON, LLM) ! UPWARD EFFECTIVE CLOUD COVER
+    DOUBLE PRECISION DP(KLON, LLM) ! LAYER PRESSURE THICKNESS (Pa)
+    DOUBLE PRECISION DT0(KLON) ! SURFACE TEMPERATURE DISCONTINUITY (K)
+    DOUBLE PRECISION EMIS(KLON) ! SURFACE EMISSIVITY
+    DOUBLE PRECISION PMB(KLON, LLM+1) ! HALF LEVEL PRESSURE (mb)
+    DOUBLE PRECISION OZON(KLON, LLM) ! O3 CONCENTRATION (kg/kg)
+    DOUBLE PRECISION TL(KLON, LLM+1) ! HALF LEVEL TEMPERATURE (K)
 
-    DOUBLE PRECISION PTAVE(KLON, LLM) ! LAYER TEMPERATURE (K)
-    DOUBLE PRECISION PVIEW(KLON) ! COSECANT OF VIEWING ANGLE
-    DOUBLE PRECISION PWV(KLON, LLM) ! SPECIFIC HUMIDITY (kg/kg)
+    DOUBLE PRECISION TAVE(KLON, LLM) ! LAYER TEMPERATURE (K)
+    DOUBLE PRECISION VIEW(KLON) ! COSECANT OF VIEWING ANGLE
+    DOUBLE PRECISION WV(KLON, LLM) ! SPECIFIC HUMIDITY (kg/kg)
 
-    DOUBLE PRECISION PCOLR(KLON, LLM) ! LONG-WAVE TENDENCY (K/day)
-    DOUBLE PRECISION PCOLR0(KLON, LLM) ! LONG-WAVE TENDENCY (K/day) clear-sky
-    DOUBLE PRECISION PTOPLW(KLON) ! LONGWAVE FLUX AT T.O.A.
-    DOUBLE PRECISION PSOLLW(KLON) ! LONGWAVE FLUX AT SURFACE
-    DOUBLE PRECISION PTOPLW0(KLON) ! LONGWAVE FLUX AT T.O.A. (CLEAR-SKY)
-    DOUBLE PRECISION PSOLLW0(KLON) ! LONGWAVE FLUX AT SURFACE (CLEAR-SKY)
+    DOUBLE PRECISION COLR(KLON, LLM) ! LONG-WAVE TENDENCY (K/day)
+    DOUBLE PRECISION COLR0(KLON, LLM) ! LONG-WAVE TENDENCY (K/day) clear-sky
+    DOUBLE PRECISION TOPLW(KLON) ! LONGWAVE FLUX AT T.O.A.
+    DOUBLE PRECISION SOLLW(KLON) ! LONGWAVE FLUX AT SURFACE
+    DOUBLE PRECISION TOPLW0(KLON) ! LONGWAVE FLUX AT T.O.A. (CLEAR-SKY)
+    DOUBLE PRECISION SOLLW0(KLON) ! LONGWAVE FLUX AT SURFACE (CLEAR-SKY)
     ! Rajout LF
-    double precision psollwdown(klon) ! LONGWAVE downwards flux at surface
+    double precision sollwdown(klon) ! LONGWAVE downwards flux at surface
     !IM
-    DOUBLE PRECISION plwup(KLON, LLM+1) ! LW up total sky
-    DOUBLE PRECISION plwup0(KLON, LLM+1) ! LW up clear sky
-    DOUBLE PRECISION plwdn(KLON, LLM+1) ! LW down total sky
-    DOUBLE PRECISION plwdn0(KLON, LLM+1) ! LW down clear sky
+    DOUBLE PRECISION lwup(KLON, LLM+1) ! LW up total sky
+    DOUBLE PRECISION lwup0(KLON, LLM+1) ! LW up clear sky
+    DOUBLE PRECISION lwdn(KLON, LLM+1) ! LW down total sky
+    DOUBLE PRECISION lwdn0(KLON, LLM+1) ! LW down clear sky
 
     DOUBLE PRECISION ZABCU(KLON, NUA, 3*LLM+1)
     DOUBLE PRECISION ZOZ(KLON, LLM)
@@ -105,19 +105,19 @@ contains
        DO k = 1, LLM
           DO i = 1, KLON
              ! convertir ozone de kg/kg en pa (modif MPL 100505)
-             ZOZ(i, k) = POZON(i, k)*PDP(i, k) * MD/RMO3
+             ZOZ(i, k) = OZON(i, k)*DP(i, k) * MD/RMO3
           ENDDO
        ENDDO
 
-       CALL LWU(PDP, PPMB, ZOZ, PTAVE, PVIEW, PWV, ZABCU)
-       CALL LWBV(ILIM, PDT0, PEMIS, PPMB, PTL, PTAVE, ZABCU, &
+       CALL LWU(DP, PMB, ZOZ, TAVE, VIEW, WV, ZABCU)
+       CALL LWBV(ILIM, DT0, EMIS, PMB, TL, TAVE, ZABCU, &
             ZFLUC, ZBINT, ZBSUI, ZCTS, ZCNTRB)
        itaplw0 = 0
     ENDIF
     itaplw0 = itaplw0 + 1
 
     IF (MOD(itaplw, lwpas) == 0) THEN
-       CALL LWC(ILIM, PCLDLD, PCLDLU, PEMIS, &
+       CALL LWC(ILIM, CLDLD, CLDLU, EMIS, &
             ZFLUC, ZBINT, ZBSUI, ZCTS, ZCNTRB, &
             ZFLUX)
        itaplw = 0
@@ -127,28 +127,28 @@ contains
     DO k = 1, LLM
        kpl1 = k+1
        DO i = 1, KLON
-          PCOLR(i, k) = ZFLUX(i, 1, kpl1)+ZFLUX(i, 2, kpl1) &
+          COLR(i, k) = ZFLUX(i, 1, kpl1)+ZFLUX(i, 2, kpl1) &
                - ZFLUX(i, 1, k)- ZFLUX(i, 2, k)
-          PCOLR(i, k) = PCOLR(i, k) * DAYSEC*RG/RCPD / PDP(i, k)
-          PCOLR0(i, k) = ZFLUC(i, 1, kpl1)+ZFLUC(i, 2, kpl1) &
+          COLR(i, k) = COLR(i, k) * DAYSEC*RG/RCPD / DP(i, k)
+          COLR0(i, k) = ZFLUC(i, 1, kpl1)+ZFLUC(i, 2, kpl1) &
                - ZFLUC(i, 1, k)- ZFLUC(i, 2, k)
-          PCOLR0(i, k) = PCOLR0(i, k) * DAYSEC*RG/RCPD / PDP(i, k)
+          COLR0(i, k) = COLR0(i, k) * DAYSEC*RG/RCPD / DP(i, k)
        ENDDO
     ENDDO
     DO i = 1, KLON
-       PSOLLW(i) = -ZFLUX(i, 1, 1)-ZFLUX(i, 2, 1)
-       PTOPLW(i) = ZFLUX(i, 1, LLM+1) + ZFLUX(i, 2, LLM+1)
+       SOLLW(i) = -ZFLUX(i, 1, 1)-ZFLUX(i, 2, 1)
+       TOPLW(i) = ZFLUX(i, 1, LLM+1) + ZFLUX(i, 2, LLM+1)
 
-       PSOLLW0(i) = -ZFLUC(i, 1, 1)-ZFLUC(i, 2, 1)
-       PTOPLW0(i) = ZFLUC(i, 1, LLM+1) + ZFLUC(i, 2, LLM+1)
-       psollwdown(i) = -ZFLUX(i, 2, 1)
+       SOLLW0(i) = -ZFLUC(i, 1, 1)-ZFLUC(i, 2, 1)
+       TOPLW0(i) = ZFLUC(i, 1, LLM+1) + ZFLUC(i, 2, LLM+1)
+       sollwdown(i) = -ZFLUX(i, 2, 1)
 
        ! Attention aux signes : LWtop > 0, LWdn < 0
        DO k = 1, LLM+1
-          plwup(i, k) = ZFLUX(i, 1, k)
-          plwup0(i, k) = ZFLUC(i, 1, k)
-          plwdn(i, k) = ZFLUX(i, 2, k)
-          plwdn0(i, k) = ZFLUC(i, 2, k)
+          lwup(i, k) = ZFLUX(i, 1, k)
+          lwup0(i, k) = ZFLUC(i, 1, k)
+          lwdn(i, k) = ZFLUX(i, 2, k)
+          lwdn0(i, k) = ZFLUC(i, 2, k)
        ENDDO
     ENDDO
 
