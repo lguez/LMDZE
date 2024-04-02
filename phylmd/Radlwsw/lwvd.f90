13c029f1008445d0f9f773292d21bfbbee9fbe6d
Lionel GUEZ <guez@lmd.ens.fr>
2024-04-02 16:54:28 +0200

    Polish
    With `polish_Fortran.sh`.

--- phylmd/Radlwsw/lwvd.f90
+++ phylmd/Radlwsw/lwvd.f90
@@ -9,31 +9,28 @@ contains
     USE dimphy
     USE raddimlw
 
-    ! -----------------------------------------------------------------------
     ! PURPOSE.
-    ! --------
+
     ! CARRIES OUT THE VERTICAL INTEGRATION ON THE DISTANT LAYERS
 
     ! METHOD.
-    ! -------
 
     ! 1. PERFORMS THE VERTICAL INTEGRATION CORRESPONDING TO THE
     ! CONTRIBUTIONS OF THE DISTANT LAYERS USING TRAPEZOIDAL RULE
 
     ! REFERENCE.
-    ! ----------
 
     ! SEE RADIATION'S PART OF THE MODEL'S DOCUMENTATION AND
     ! ECMWF RESEARCH DEPARTMENT DOCUMENTATION OF THE IFS
 
     ! AUTHOR.
-    ! -------
-    ! JEAN-JACQUES MORCRETTE  *ECMWF*
+
+    ! JEAN-JACQUES MORCRETTE *ECMWF*
 
     ! MODIFICATIONS.
-    ! --------------
+
     ! ORIGINAL : 89-07-14
-    ! -----------------------------------------------------------------------
+
     ! * ARGUMENTS:
 
     INTEGER ktraer
@@ -44,8 +41,8 @@ contains
     DOUBLE PRECISION pgb(klon, 8, 2, llm) ! PADE APPROXIMANTS
 
     DOUBLE PRECISION pcntrb(klon, llm+1, llm+1) ! ENERGY EXCHANGE MATRIX
-    DOUBLE PRECISION pdisd(klon, llm+1) !  CONTRIBUTION BY DISTANT LAYERS
-    DOUBLE PRECISION pdisu(klon, llm+1) !  CONTRIBUTION BY DISTANT LAYERS
+    DOUBLE PRECISION pdisd(klon, llm+1) ! CONTRIBUTION BY DISTANT LAYERS
+    DOUBLE PRECISION pdisu(klon, llm+1) ! CONTRIBUTION BY DISTANT LAYERS
 
     ! * LOCAL VARIABLES:
 
@@ -60,13 +57,9 @@ contains
     INTEGER itt
     DOUBLE PRECISION zww, zdzxdg, zdzxmg
 
-    ! *         1.    INITIALIZATION
-    ! --------------
+    ! * 1. INITIALIZATION
 
-
-    ! *         1.1     INITIALIZE LAYER CONTRIBUTIONS
-    ! ------------------------------
-
+    ! * 1.1 INITIALIZE LAYER CONTRIBUTIONS
 
     DO jk = 1, llm + 1
        DO jl = 1, klon
@@ -75,10 +68,7 @@ contains
        END DO
     END DO
 
-    ! *         1.2     INITIALIZE TRANSMISSION FUNCTIONS
-    ! ---------------------------------
-
-
+    ! * 1.2 INITIALIZE TRANSMISSION FUNCTIONS
 
     DO ja = 1, ntra
        DO jl = 1, klon
@@ -88,39 +78,23 @@ contains
        END DO
     END DO
 
-    ! ------------------------------------------------------------------
+    ! * 2. VERTICAL INTEGRATION
 
-    ! *         2.      VERTICAL INTEGRATION
-    ! --------------------
+    ! * 2.2 CONTRIBUTION FROM DISTANT LAYERS
 
-    ! *         2.2     CONTRIBUTION FROM DISTANT LAYERS
-    ! ---------------------------------
+    ! * 2.2.1 DISTANT AND ABOVE LAYERS
 
-
-
-    ! *         2.2.1   DISTANT AND ABOVE LAYERS
-    ! ------------------------
-
-
-
-
-    ! *         2.2.2   FIRST UPPER LEVEL
-    ! -----------------
-
+    ! * 2.2.2 FIRST UPPER LEVEL
 
     DO jk = 1, llm - 1
        ikp1 = jk + 1
        ikn = (jk-1)*ng1p1 + 1
        ikd1 = jk*ng1p1 + 1
 
-       CALL lwttm(pga(1,1,1,jk), pgb(1,1,1,jk), pabcu(1,1,ikn), pabcu(1,1,ikd1), &
+       CALL lwttm(pga(1, 1, 1, jk), pgb(1, 1, 1, jk), pabcu(1, 1, ikn), pabcu(1, 1, ikd1), &
             ztt1)
 
-
-
-       ! *         2.2.3   HIGHER UP
-       ! ---------
-
+       ! * 2.2.3 HIGHER UP
 
        itt = 1
        DO jkj = ikp1, llm
@@ -133,16 +107,16 @@ contains
           ikd2 = jkj*ng1p1 + 1
 
           IF (itt==1) THEN
-             CALL lwttm(pga(1,1,1,jkj), pgb(1,1,1,jkj), pabcu(1,1,ikn), &
-                  pabcu(1,1,ikd2), ztt1)
+             CALL lwttm(pga(1, 1, 1, jkj), pgb(1, 1, 1, jkj), pabcu(1, 1, ikn), &
+                  pabcu(1, 1, ikd2), ztt1)
           ELSE
-             CALL lwttm(pga(1,1,1,jkj), pgb(1,1,1,jkj), pabcu(1,1,ikn), &
-                  pabcu(1,1,ikd2), ztt2)
+             CALL lwttm(pga(1, 1, 1, jkj), pgb(1, 1, 1, jkj), pabcu(1, 1, ikn), &
+                  pabcu(1, 1, ikd2), ztt2)
           END IF
 
           DO ja = 1, ktraer
              DO jl = 1, klon
-                ztt(jl, ja) = (ztt1(jl,ja)+ztt2(jl,ja))*0.5
+                ztt(jl, ja) = (ztt1(jl, ja)+ztt2(jl, ja))*0.5
              END DO
           END DO
 
@@ -159,20 +133,12 @@ contains
              pcntrb(jl, jk, ikjp1) = zdzxdg
           END DO
 
-
        END DO
     END DO
 
+    ! * 2.2.4 DISTANT AND BELOW LAYERS
 
-    ! *         2.2.4   DISTANT AND BELOW LAYERS
-    ! ------------------------
-
-
-
-
-    ! *         2.2.5   FIRST LOWER LEVEL
-    ! -----------------
-
+    ! * 2.2.5 FIRST LOWER LEVEL
 
     DO jk = 3, llm + 1
        ikn = (jk-1)*ng1p1 + 1
@@ -180,15 +146,10 @@ contains
        ikj = jk - 2
        iku1 = ikj*ng1p1 + 1
 
+       CALL lwttm(pga(1, 1, 1, ikj), pgb(1, 1, 1, ikj), pabcu(1, 1, iku1), &
+            pabcu(1, 1, ikn), ztt1)
 
-       CALL lwttm(pga(1,1,1,ikj), pgb(1,1,1,ikj), pabcu(1,1,iku1), &
-            pabcu(1,1,ikn), ztt1)
-
-
-
-       ! *         2.2.6   DOWN BELOW
-       ! ----------
-
+       ! * 2.2.6 DOWN BELOW
 
        itt = 1
        DO jlk = 1, ikj
@@ -200,18 +161,17 @@ contains
           ijkl = ikm1 - jlk
           iku2 = (ijkl-1)*ng1p1 + 1
 
-
           IF (itt==1) THEN
-             CALL lwttm(pga(1,1,1,ijkl), pgb(1,1,1,ijkl), pabcu(1,1,iku2), &
-                  pabcu(1,1,ikn), ztt1)
+             CALL lwttm(pga(1, 1, 1, ijkl), pgb(1, 1, 1, ijkl), pabcu(1, 1, iku2), &
+                  pabcu(1, 1, ikn), ztt1)
           ELSE
-             CALL lwttm(pga(1,1,1,ijkl), pgb(1,1,1,ijkl), pabcu(1,1,iku2), &
-                  pabcu(1,1,ikn), ztt2)
+             CALL lwttm(pga(1, 1, 1, ijkl), pgb(1, 1, 1, ijkl), pabcu(1, 1, iku2), &
+                  pabcu(1, 1, ikn), ztt2)
           END IF
 
           DO ja = 1, ktraer
              DO jl = 1, klon
-                ztt(jl, ja) = (ztt1(jl,ja)+ztt2(jl,ja))*0.5
+                ztt(jl, ja) = (ztt1(jl, ja)+ztt2(jl, ja))*0.5
              END DO
           END DO
 
@@ -227,7 +187,6 @@ contains
              pdisu(jl, jk) = pdisu(jl, jk) + zdzxmg
              pcntrb(jl, jk, ijkl) = zdzxmg
           END DO
-
 
        END DO
     END DO
